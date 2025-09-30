c909ff4c0b2a786a421145e665eb88e4b5dadb75
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-30 21:05:03 +0700

    feat(configs/redis): add redis config

--- internal/configs/redis.go
+++ internal/configs/redis.go
@@ -1,1 +1,28 @@
 package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func InitRedis() (*redis.Client, error) {
+	rdbHost := os.Getenv("RDBHOST")
+	rdbPort := os.Getenv("RDBPORT")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", rdbHost, rdbPort),
+		Password: "",
+		DB:       0,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, err
+	} else {
+		log.Println("RDB Connected")
+	}
+
+	return rdb, nil
+}
