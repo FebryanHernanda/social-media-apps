01a9fdfffc51d7ecaad361180b590c6bf56b08ee
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-30 21:05:29 +0700

    feat(router): config router

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,1 +1,13 @@
 package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+func Router(db *pgxpool.Pool, rdb *redis.Client) *gin.Engine {
+	r := gin.Default()
+
+	return r
+}
