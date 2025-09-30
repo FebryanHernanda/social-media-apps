7a257916071c8804678aaa0c834a910b42c90f03
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-30 21:05:50 +0700

    feat(main): setup db, redis and router

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"net/http"
+	"log"
 
-	"github.com/gin-gonic/gin"
+	"github.com/febryanhernanda/social-media-apps/internal/configs"
+	"github.com/febryanhernanda/social-media-apps/internal/routers"
 )
 
 func main() {
-	r := gin.Default()
+	db, err := configs.InitDB()
+	if err != nil {
+		log.Fatal("DB init failed: ", err)
+	}
+	defer db.Close()
 
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	rdb, err := configs.InitRedis()
+	if err != nil {
+		log.Fatal("RDB init failed: ", err)
+	}
+
+	if rdb != nil {
+		defer rdb.Close()
+	}
+
+	r := routers.Router(db, rdb)
 
 	r.Run(":8080")
 }
